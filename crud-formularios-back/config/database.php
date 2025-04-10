8fe7fde11c2bc55df3c6b7ce4876ecc35834370a
Pedro <pedrojuan36022@gmail.com>
2025-04-10 18:26:28 +0200

    Configurado el archivo database.php para que se use mongoDB

--- crud-formularios-back/config/database.php
+++ crud-formularios-back/config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'mongodb'),
 
     /*
     |--------------------------------------------------------------------------
@@ -111,8 +111,19 @@ return [
             // 'encrypt' => env('DB_ENCRYPT', 'yes'),
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
+        'mongodb' => [
+        'driver'   => 'mongodb',
+        'host'     => env('DB_HOST', '127.0.0.1'),
+        'port'     => env('DB_PORT', 27017),
+        'database' => env('DB_DATABASE', 'your_database_name'),
+        'username' => env('DB_USERNAME', ''),
+        'password' => env('DB_PASSWORD', ''),
+        'options'  => [
+            'database' => env('DB_AUTH_DATABASE', 'admin'), // set the authentication database
+        ],
 
     ],
+],
 
     /*
     |--------------------------------------------------------------------------
