4887a962216f437188bf8beac310f1001b1e0440
Pedro <pedrojuan36022@gmail.com>
2025-04-11 14:18:49 +0200

    cambio menor en api.php

--- crud-formularios-back/routes/api.php
+++ crud-formularios-back/routes/api.php
@@ -14,5 +14,4 @@ Route::get('/forms', [FormController::class, 'getForms']);
 Route::get('/forms/{id}', [FormController::class, 'getForm']);
 Route::post('/forms', [FormController::class, 'createForm']);
 Route::put('/forms/{id}', [FormController::class, 'updateForm']);
-Route::put('/forms/{id}', [FormController::class, 'updateForm']);
 Route::delete('/forms/{id}', [FormController::class, 'deleteForm']);
